53e610effefcaa99d771fbb01a16ee6a91c27966
waynevivek <vivek.neuralvibe@gmail.com>
2026-02-24 23:37:38 +0530

    first commit

---
+++ script_01.js
@@ -0,0 +1,55 @@
+// if variable is  known but containes nothing (no value or null) = undefined (datatype nhi hota  - nit initialise )
+/* 
+null is also a value 
+--- let const and var (names for the variables)
+--- local and global - local is much better (let and const ) ;
+    global(var) takes moe memory space and will remain till the program ends 
+
+const: give value at hte time of initialisation; no redeclaration;
+primitive type ; null and undefined , boolnean , string , number ,
+
+reference type ; array and function and object;
+
+array ; new array (memory show krega)
+
+object = key value pair;
+   nmae (key) = Vivek(value);
+
+Symbol type === new type of primitive data type ( clashes ambiguity --scorp resolution (unique identification)) ;
+
+operators(javascript) == arithmetic , 
+
+html ko js me kaie likhenge == using it under double quoutes "";
+
+= ; assign value 
+== ; comparison irrespective of the data type 
+=== ; data type ko comapre ( strict type checking )
+
+assignment operator 
+ =
+ +=  ( a += 2 ; a = a + 2)
+ -=
+remember that after applying these operators the values changes simultaneously 
+
+logical operators 
+ &&
+ || 
+ ! 
+
+
+
+
+*/
+
+// maximum of three numbers using ternary operator 
+
+
+ var num = 10 
+
+ console.log(num);
+ console.log(typeof(num));
+
+ 
+
+
+
